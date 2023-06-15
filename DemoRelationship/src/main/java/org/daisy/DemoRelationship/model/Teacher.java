f98bc97ee7e18c3be0ad5746063c58810e530561
Daisy <daisy.le0203@gmail.com>
2023-06-15 10:22:07 -0500

    Demonstration of @ManytoMany Relationship and Mapping

--- DemoRelationship/src/main/java/org/daisy/DemoRelationship/model/Teacher.java
+++ DemoRelationship/src/main/java/org/daisy/DemoRelationship/model/Teacher.java
@@ -1,30 +1,44 @@
 package org.daisy.DemoRelationship.model;
 
-import java.io.Serializable;
-import jakarta.persistence.*;
-
+import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table
-public class Teacher implements Serializable  {
-	private static final long serialVersionUID = 1L;
+public class Teacher {
 	@Id
-	@GeneratedValue( strategy=GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int tid;
 	private String salary;
 	private String Teachername;
-	
+
 //	@ManyToOne
 //	private Department department;
-	public Teacher(int tid, String salary, String teachername) {
-		super();
-		this.tid = tid;
+//	public Teacher(int tid, String salary, String teachername) {
+//		super();
+//		this.tid = tid;
+//		this.salary = salary;
+//		Teachername = teachername;
+//	}
+
+	@ManyToMany(targetEntity = Cohort.class)
+	private Set CohortSet;
+
+	public Teacher(String salary, String teachername, Set CohortSet) {
 		this.salary = salary;
-		Teachername = teachername;
+		this.Teachername = teachername;
+		this.CohortSet = CohortSet;
+
 	}
-	public Teacher()
-	{}
-	
+
+	public Teacher() {
+	}
+
 //	public Department getDep() {
 //		return department;
 //	}
@@ -34,19 +48,32 @@ public class Teacher implements Serializable  {
 	public int gettid() {
 		return tid;
 	}
+
 	public void settid(int tid) {
 		this.tid = tid;
 	}
+
+	public Set getCohortSet() {
+		return CohortSet;
+	}
+
+	public void setCohortSet(Set cohortSet) {
+		CohortSet = cohortSet;
+	}
+
 	public String getSalary() {
 		return salary;
 	}
+
 	public void setSalary(String salary) {
 		this.salary = salary;
 	}
+
 	public String getTeachername() {
 		return Teachername;
 	}
+
 	public void setTeachername(String teachername) {
 		Teachername = teachername;
-	}	
+	}
 }
