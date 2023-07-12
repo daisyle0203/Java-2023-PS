9cba497cd4bacf935eeab5fc8155d8f24f590a97
Daisy <daisy.le0203@gmail.com>
2023-07-12 15:35:50 -0500

    spring boot student api demo

---
+++ BeginnersBookDemo/src/main/java/servlets/MyServletDemo.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServletDemo extends HttpServlet {
+	private String mymsg;
+
+	public void init() throws ServletException {
+		mymsg = "Hello World!";
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Setting up the content type of webpage
+		response.setContentType("text/html");
+
+		// Writing message to the web page
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + mymsg + "</h1>");
+	}
+
+	public void destroy() {
+		/*
+		 * leaving empty for now this can be used when we want to do something at the
+		 * end of Servlet life cycle
+		 */
+	}
+
+}
